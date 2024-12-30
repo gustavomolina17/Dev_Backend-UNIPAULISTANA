8f979986ac14fad8dd0c760cbb201f01d932f748
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2024-12-30 10:19:27 -0300

    Create vingadores.java

---
+++ 07 - Códigos/00 - Revisão OO/vingadores.java
@@ -0,0 +1,49 @@
+package Aula_01;
+
+// Criação da classe (Modelo)
+public class vingadores{
+
+    // Criação dos atributos
+
+   String nome, sexo, habilidade;
+   int idade;
+   double altura;
+ 
+   // Métodos (ações)
+
+   // Método void (não tem retorno numérico)
+
+    void salvar (){
+
+        System.out.println("O vingador está salvando a senhora indefesa");
+        
+    }
+
+    void destruir(){
+
+        System.out.println("O vingador está destruindo a nave inimiga");
+    }
+
+    void correr(){
+
+        System.out.println("O vingador está correndo da organizada do Corinthians");
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
