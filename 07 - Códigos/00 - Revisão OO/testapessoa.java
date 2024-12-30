14955e224101f82d07a521fed8b7a2015ba5a56c
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2024-12-30 10:19:18 -0300

    Create testapessoa.java

---
+++ 07 - Códigos/00 - Revisão OO/testapessoa.java
@@ -0,0 +1,20 @@
+package Aula_01;
+
+public class testapessoa {
+
+    public static void main(String[] args) {
+
+        pessoa p1 = new pessoa();
+        p1.setNome("Laura");
+        p1.setIdade(18);
+
+        System.out.println("Nome: "+p1.getNome());
+        System.out.println("Idade: "+p1.getIdade());
+        
+    }
+
+
+    
+
+
+}
