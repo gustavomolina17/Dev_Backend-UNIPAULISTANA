e75883878c7567692bf6439dc8ead7e6f7854036
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2024-12-30 10:20:47 -0300

    Create Livro.java

---
+++ 07 - Códigos/04 - Gerenciador de Livros MVC/Model/Livro.java
@@ -0,0 +1,29 @@
+package Model;
+
+/**
+ *
+ * @author professor
+ */
+public class Livro {
+    private String titulo;
+    private String autor;
+    
+    public Livro(String titulo,String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+    }   
+
+public String getTitulo(){
+return titulo;
+}
+
+public String getAutor(){
+return autor;
+}
+
+@Override
+public String toString(){
+    return titulo + "-" +autor;
+}
+}
+
