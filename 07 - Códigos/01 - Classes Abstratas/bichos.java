d7acf2e47a1588677571d6921366bb0955e5cc21
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2024-12-30 10:19:36 -0300

    Create bichos.java

---
+++ 07 - Códigos/01 - Classes Abstratas/bichos.java
@@ -0,0 +1,17 @@
+package Aula_02;
+
+public class bichos {
+
+    public static void main(String[] args) {
+
+        furao fernando = new furao();
+        hiena banzai = new hiena();
+        gorila kingkong = new gorila();
+
+        fernando.movimento();
+        banzai.movimento();
+        kingkong.movimento();
+
+    }
+    
+}
