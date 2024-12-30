9fcbc80e7aaf5b41dffb9a8566b38e715ae453d4
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2024-12-30 10:19:33 -0300

    Create animal.java

---
+++ 07 - Códigos/01 - Classes Abstratas/animal.java
@@ -0,0 +1,11 @@
+package Aula_02;
+
+public abstract class animal {
+    protected String nome;
+    protected String cor;
+    protected int idade;
+    protected double peso;
+
+    public abstract void movimento();
+    
+}
